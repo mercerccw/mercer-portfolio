4cd7a68dbbb835ec0201731639b426fda903b691
Clayton Mercer <mercerccw@gmail.com>
2021-01-12 14:02:53 -0500

    Add Node-Sass

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,6 +1,29 @@
 import React from "react"
 import "./Projects.scss"
+import Alert from "react-bootstrap/cjs/Alert"
+
+function generateAlerts() {
+  return([
+    'primary',
+    'secondary',
+    'success',
+    'danger',
+    'warning',
+    'info',
+    'light',
+    'dark',
+  ].map((variant, idx) => (
+    <Alert key={idx} variant={variant}>
+      This is a {variant} alert—check it out!
+    </Alert>
+  )));
+}
 
 export default function Projects() {
-  return <div>Projects Page!</div>
+  return <>
+    {generateAlerts()}
+    <Alert variant='danger'>
+      This is an alert—check it out!
+    </Alert>
+  </>
 }
