403aeaf8a82daf5e8d74745301beec4ff1617106
Clayton Mercer <mercerccw@gmail.com>
2021-02-16 15:40:17 -0500

    Add homepage layout

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,6 @@ function App() {
           </Route>
         </Switch>
       </Router>
-      <Footer />
     </div>
 
   )
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,6 +1,116 @@
 import React from "react"
 import "./Home.scss"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+import clayton from "../../images/clayton.png"
+import clayton2 from "../../images/clayton2.png"
+import working from "../../images/working.png"
+import working2 from "../../images/working2.png"
+import arrow from "../../images/arrowup.png"
+import { Footer } from "../Footer"
 
 export const Home = () => {
-  return <div>Home Page!</div>
+  return (
+    <section className="home">
+
+      <section className="title-card" style={{
+        backgroundImage: `url("https://mercer-portfolio-assets.s3.us-east-2.amazonaws.com/hero_image.jpeg")`
+      }}>
+        <div className="title">
+          <h1>Clayton Mercer</h1>
+          <h2>Software Developer</h2>
+        </div>
+      </section>
+      <div style={{
+        backgroundColor: "white",
+        borderRadius: "50px",
+        width: "fit-content",
+        marginLeft: "auto",
+        marginRight: "auto",
+        left: 0,
+        right: 0,
+        textAlign: "center",
+        zIndex: 1000,
+        marginTop: "-60px",
+        position: "absolute",
+        boxShadow: "3px 3px lightgray"
+      }}>
+        <a href="#main-content">
+          <img src={arrow} alt="arrow" width="100px" style={{
+            display: "flex",
+            justifyContent: "center",
+            textAlign: "center",
+            transform: "rotate(180deg)",
+          }}/>
+        </a>
+
+      </div>
+
+      <section id="main-content" className="main-content">
+        <div className="divider">
+          <span className="ribbon-divider"></span>
+        </div>
+        <article className="content-image">
+          <div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque illo laborum, minima nesciunt omnis
+              possimus
+              quasi sed. Consequuntur cumque ipsa iusto libero modi nulla odio pariatur quo, repellat voluptate?
+              Ducimus!</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque illo laborum, minima nesciunt omnis
+              possimus
+              quasi sed. Consequuntur cumque ipsa iusto libero modi nulla odio pariatur quo, repellat voluptate?
+              Ducimus!</p>
+          </div>
+          <img src={clayton} alt="clayton"/>
+        </article>
+        <article className="content-image-mobile">
+          <div className="background-image">
+            <div className="ribbon"></div>
+            <img src={clayton2} alt="clayton"/>
+          </div>
+          <div className="content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque illo laborum, minima nesciunt omnis
+              possimus
+              quasi sed. Consequuntur cumque ipsa iusto libero modi nulla odio pariatur quo, repellat voluptate?
+              Ducimus!</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque illo laborum, minima nesciunt omnis
+              possimus
+              quasi sed. Consequuntur cumque ipsa iusto libero modi nulla odio pariatur quo, repellat voluptate?
+              Ducimus!</p>
+          </div>
+        </article>
+        <div className="divider"><span className="ribbon-divider"></span></div>
+        <article className="content-image">
+          <img src={working} alt="clayton"/>
+            <div>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque illo laborum, minima nesciunt omnis
+                possimus
+                quasi sed. Consequuntur cumque ipsa iusto libero modi nulla odio pariatur quo, repellat voluptate?
+                Ducimus!</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque illo laborum, minima nesciunt omnis
+                possimus
+                quasi sed. Consequuntur cumque ipsa iusto libero modi nulla odio pariatur quo, repellat voluptate?
+                Ducimus!</p>
+            </div>
+        </article>
+        <article className="content-image-mobile">
+          <div className="background-image">
+            <div className="ribbon"></div>
+            <img src={working2} alt="clayton"/>
+          </div>
+          <div className="content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque illo laborum, minima nesciunt omnis
+              possimus
+              quasi sed. Consequuntur cumque ipsa iusto libero modi nulla odio pariatur quo, repellat voluptate?
+              Ducimus!</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque illo laborum, minima nesciunt omnis
+              possimus
+              quasi sed. Consequuntur cumque ipsa iusto libero modi nulla odio pariatur quo, repellat voluptate?
+              Ducimus!</p>
+          </div>
+        </article>
+      </section>
+      <Footer />
+    </section>
+  )
+
 }
