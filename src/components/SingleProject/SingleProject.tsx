5d7173f41c9efa04b36e5aacece7317ffa823535
Clayton Mercer <mercerccw@gmail.com>
2021-02-19 11:12:54 -0500

    Fix project links

--- src/components/SingleProject/SingleProject.tsx
+++ src/components/SingleProject/SingleProject.tsx
@@ -89,7 +89,7 @@ export const SingleProject = () => {
                   {
                     project.links.map((link) => {
                       return (
-                        <li key={link.name}><a href={link.link}>{link.name}</a></li>
+                        <li key={link.name}><a href={link.link}  target="_blank" rel="noopener noreferrer">{link.name}</a></li>
                       )
                     })
                   }
