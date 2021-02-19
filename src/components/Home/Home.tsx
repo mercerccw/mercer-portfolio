a2812cd5eb6337c24f87dd1730adc13126392ae6
Clayton Mercer <mercerccw@gmail.com>
2021-02-18 20:40:10 -0500

    Finish Homepage content

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,17 +1,21 @@
 import React from "react"
 import "./Home.scss"
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
-import clayton from "../../images/clayton.png"
-import clayton2 from "../../images/clayton2.png"
-import working from "../../images/working.png"
-import working2 from "../../images/working2.png"
+import cloudSymbol from "../../images/cloud-symbol.png"
+import backendSymbol from "../../images/backend-symbol.png"
+import dataBaseSymbol from "../../images/database-symbol.png"
+import frontendSymbol from "../../images/frontend-symbol.png"
+import appleLogo from "../../images/apple-logo.png"
+import androidLogo from "../../images/android-logo.png"
+import unityLogo from "../../images/unity-logo.png"
+import slackLogo from "../../images/slack-logo.png"
+import teamsLogo from "../../images/microsoft-teams-logo.png"
 import arrow from "../../images/arrowup.png"
 import { Footer } from "../Footer"
+import { Link } from "react-router-dom"
 
 export const Home = () => {
   return (
     <section className="home">
-
       <section className="title-card">
         <div className="title">
           <h1>Clayton Mercer</h1>
@@ -19,72 +23,68 @@ export const Home = () => {
         </div>
         <div id="down-button">
           <a href="#main-content">
-            <img src={arrow} alt="arrow"/>
+            <img src={arrow} alt="arrow" id="main-content"/>
           </a>
         </div>
       </section>
-
-
-      <section id="main-content" className="main-content">
-        <article className="content-image">
-          <div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque illo laborum, minima nesciunt omnis
-              possimus
-              quasi sed. Consequuntur cumque ipsa iusto libero modi nulla odio pariatur quo, repellat voluptate?
-              Ducimus!</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque illo laborum, minima nesciunt omnis
-              possimus
-              quasi sed. Consequuntur cumque ipsa iusto libero modi nulla odio pariatur quo, repellat voluptate?
-              Ducimus!</p>
-          </div>
-          <img src={clayton} alt="clayton"/>
-        </article>
-        <article className="content-image-mobile">
-          <div className="background-image">
-            <div className="ribbon"></div>
-            <img src={clayton2} alt="clayton"/>
-          </div>
-          <div className="content">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque illo laborum, minima nesciunt omnis
-              possimus
-              quasi sed. Consequuntur cumque ipsa iusto libero modi nulla odio pariatur quo, repellat voluptate?
-              Ducimus!</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque illo laborum, minima nesciunt omnis
-              possimus
-              quasi sed. Consequuntur cumque ipsa iusto libero modi nulla odio pariatur quo, repellat voluptate?
-              Ducimus!</p>
-          </div>
-        </article>
-        <div className="divider"><span className="ribbon-divider"></span></div>
-        <article className="content-image">
-          <img src={working} alt="clayton"/>
-            <div>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque illo laborum, minima nesciunt omnis
-                possimus
-                quasi sed. Consequuntur cumque ipsa iusto libero modi nulla odio pariatur quo, repellat voluptate?
-                Ducimus!</p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque illo laborum, minima nesciunt omnis
-                possimus
-                quasi sed. Consequuntur cumque ipsa iusto libero modi nulla odio pariatur quo, repellat voluptate?
-                Ducimus!</p>
+      <section className="home-content">
+        <div className="experience">
+          <h1>Experience Summary</h1>
+          <div className="row">
+            <div className="column">
+              <h5>Full-Stack Development</h5>
+              <p>Has developed within every layer of modern web architecture.</p>
             </div>
-        </article>
-        <article className="content-image-mobile">
-          <div className="background-image">
-            <div className="ribbon"></div>
-            <img src={working2} alt="clayton"/>
+            <div className="column logo-gallery">
+              <img src={frontendSymbol} alt="" />
+              <img src={backendSymbol} alt="" />
+              <img src={dataBaseSymbol} alt="" />
+              <img src={cloudSymbol} alt="" />
+            </div>
           </div>
-          <div className="content">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque illo laborum, minima nesciunt omnis
-              possimus
-              quasi sed. Consequuntur cumque ipsa iusto libero modi nulla odio pariatur quo, repellat voluptate?
-              Ducimus!</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque illo laborum, minima nesciunt omnis
-              possimus
-              quasi sed. Consequuntur cumque ipsa iusto libero modi nulla odio pariatur quo, repellat voluptate?
-              Ducimus!</p>
+          <div className="row">
+            <div className="column">
+              <h5>Mobile Development</h5>
+              <p>Has experience building native applications built in React Native and Xamarin.</p>
+            </div>
+            <div className="column logo-gallery">
+              <img src={appleLogo} alt="" />
+              <img src={androidLogo} alt="" />
+            </div>
           </div>
-        </article>
+          <div className="row">
+            <div className="column">
+              <h5>Game Development</h5>
+              <p>Has experience developing within Unity.</p>
+            </div>
+            <div className="column logo-gallery">
+              <img src={unityLogo} alt="" />
+            </div>
+          </div>
+          <div className="row">
+            <div className="column">
+              <h5>Project Management</h5>
+              <p>Has organized and lead multiple projects.</p>
+            </div>
+            <div className="column logo-gallery">
+              <img src={slackLogo} alt="" />
+              <img src={teamsLogo} alt="" />
+            </div>
+          </div>
+          <div className="row">
+            <div className="column">
+              <h5>Soft Skills</h5>
+              <p>Prides himself in his work ethic and people skills.</p>
+            </div>
+          </div>
+          <div className="page-link">
+            <Link to={`/projects`} className="link">To Projects<img src={arrow} alt="" /></Link>
+          </div>
+          <br/>
+          <div className="page-link">
+            <Link to={`/about`} className="link">To Bio<img src={arrow} alt="" /></Link>
+          </div>
+        </div>
       </section>
       <Footer />
     </section>
